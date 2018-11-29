5b83ed0c4b67399d70f1fcd8b444948c9643eb57
mkallali <montassar.kallali@cyclope.ai>
2018-11-29 01:38:04 +0100

    fix css modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
-import  Layout from './components/Layout/Layout.js';
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import  Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-import classes from  './components/Layout/Layout.css';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout className={classes.Content}>
+        <Layout>
           <BurgerBuilder/>
         </Layout>
       </div>
